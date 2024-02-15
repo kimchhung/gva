38a0d6c5162535edc3461705a8a4e36ea0892dfc
chhung <pechkimchhung@gmail.com>
2024-02-14 20:50:31 -0500

    update

--- api/config/config.go
+++ api/config/config.go
@@ -12,65 +12,59 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-type app = struct {
-	Name        string        `toml:"name"`
-	Port        string        `toml:"port"`
-	PrintRoutes bool          `toml:"print-routes"`
-	Prefork     bool          `toml:"prefork"`
-	Production  bool          `toml:"production"`
-	IdleTimeout time.Duration `toml:"idle-timeout"`
-	TLS         struct {
-		Enable   bool
-		CertFile string `toml:"cert-file"`
-		KeyFile  string `toml:"key-file"`
+type (
+	app = struct {
+		Name        string        `toml:"name"`
+		Port        string        `toml:"port"`
+		PrintRoutes bool          `toml:"print-routes"`
+		Prefork     bool          `toml:"prefork"`
+		Production  bool          `toml:"production"`
+		IdleTimeout time.Duration `toml:"idle-timeout"`
+		TLS         struct {
+			Enable   bool
+			CertFile string `toml:"cert-file"`
+			KeyFile  string `toml:"key-file"`
+		}
 	}
-}
-
-type db = struct {
-	Mysql struct {
-		DSN string `toml:"dsn"`
+	db = struct {
+		Mysql struct {
+			DSN string `toml:"dsn"`
+		}
 	}
-}
-
-type logger = struct {
-	TimeFormat string        `toml:"time-format"`
-	Level      zerolog.Level `toml:"level"`
-	Prettier   bool          `toml:"prettier"`
-}
-
-type middleware = struct {
-	Compress struct {
-		Enable bool
-		Level  compress.Level
+	logger = struct {
+		TimeFormat string        `toml:"time-format"`
+		Level      zerolog.Level `toml:"level"`
+		Prettier   bool          `toml:"prettier"`
 	}
-
-	Recover struct {
-		Enable bool
+	middleware = struct {
+		Compress struct {
+			Enable bool
+			Level  compress.Level
+		}
+		Recover struct {
+			Enable bool
+		}
+		Monitor struct {
+			Enable bool
+			Path   string
+		}
+		Pprof struct {
+			Enable bool
+		}
+		Limiter struct {
+			Enable  bool
+			Max     int
+			ExpSecs time.Duration `toml:"expiration_seconds"`
+		}
+		Filesystem struct {
+			Enable bool
+			Browse bool
+			MaxAge int `toml:"max_age"`
+			Index  string
+			Root   string
+		}
 	}
-
-	Monitor struct {
-		Enable bool
-		Path   string
-	}
-
-	Pprof struct {
-		Enable bool
-	}
-
-	Limiter struct {
-		Enable  bool
-		Max     int
-		ExpSecs time.Duration `toml:"expiration_seconds"`
-	}
-
-	Filesystem struct {
-		Enable bool
-		Browse bool
-		MaxAge int `toml:"max_age"`
-		Index  string
-		Root   string
-	}
-}
+)
 
 type Config struct {
 	App        app
