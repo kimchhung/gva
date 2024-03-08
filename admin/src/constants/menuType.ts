6efc9c4ac3afa97938506e84c868377dbc87ec2f
chhung <pechkimchhung@gmail.com>
2024-03-07 20:48:30 -0500

    seperate admin and web

---
+++ admin/src/constants/menuType.ts
@@ -0,0 +1,22 @@
+/**
+ * 菜单类型枚举
+ */
+export const enum MenuTypeEnum {
+  /**
+   * Table of contents
+   */
+  CATALOG = 'cata_log',
+  /**
+   * menu
+   */
+  MENU = 'menu',
+
+  /**
+   * Button
+   */
+  BUTTON = 'button',
+  /**
+   * External chainternal chain
+   */
+  LINK = 'external_link'
+}
