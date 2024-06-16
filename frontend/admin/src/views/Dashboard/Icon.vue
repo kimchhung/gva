777c0a54a5e3fa526b3ea685a68143be272be5b5
chhung <pechkimchhung@gmail.com>
2024-06-16 03:32:23 -0400

    add icon

---
+++ frontend/admin/src/views/Dashboard/Icon.vue
@@ -0,0 +1,10 @@
+<script setup lang="tsx">
+import { IconPicker } from '@/components/IconPicker'
+import { ContentWrap } from '@/components/ContentWrap'
+</script>
+
+<template>
+  <ContentWrap>
+    <IconPicker />
+  </ContentWrap>
+</template>
