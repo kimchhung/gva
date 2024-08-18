81627fc313435d873d4589cb88eb221b5f2d7d87
chhung <kimchhungpech@vtech.one>
2024-08-18 16:37:25 +0700

    seperate file

--- gva-backend/cmd/makefiles/api.mk
+++ gva-backend/cmd/makefiles/api.mk
@@ -1,11 +1,11 @@
 api.run.admin:
-	go run cmd/api/admin/main.go
+	go run api/admin/main.go
 
 api.run.web:
-	go run cmd/api/web/main.go 
+	go run api/web/main.go 
 
 api.run.bot:
-	go run cmd/api/bot/main.go
+	go run api/bot/main.go
 
 api.run:
-	go run cmd/api/main.go
+	go run api/main.go
